01a68b9920e412196ef40f37351000a0a12d085c
unknown <shinchanmusic24@gmail.com>
2024-05-17 13:34:20 +0530

    conditionals 04

---
+++ Conditionals/04_solution.py
@@ -0,0 +1,10 @@
+Banana = "Brown"
+
+if Banana=="Green":
+    print("Unripe")
+elif Banana == "Yellow":
+    print("ripe")
+elif Banana=="Brown":
+    print("OverRipe")
+else :
+    print("Not Recognizable!1")
