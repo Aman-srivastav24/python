2e0bb0c3c029b6c366ef263e6e234422c432139d
unknown <shinchanmusic24@gmail.com>
2024-05-17 13:56:23 +0530

    conditionals 07 and 08

---
+++ Conditionals/07_solution.py
@@ -0,0 +1,9 @@
+order_size = "Small"
+extra_shot = False
+
+if extra_shot :
+    coffee = order_size + "Coffee with an extraShot"
+else:
+    coffee = order_size+"coffee"
+
+print("Order : ", coffee)
---
+++ Conditionals/08_solution.py
@@ -0,0 +1,8 @@
+password = "nergnenon88"
+length = len(password)
+if length < 6:
+    print("Weak Password")
+elif length <10:
+    print("Medium Password")
+else : 
+    print("Strong Password")
