faa4fb14da78369b3cc099f5092a7b6021ada2be
unknown <shinchanmusic24@gmail.com>
2024-05-19 12:53:26 +0530

    09_solution

---
+++ Loops/09_solution.py
@@ -0,0 +1,10 @@
+items = ["apple", "banana", "orange", "apple", "mango" ,"mango"]
+
+unique_item = set()
+
+for item in items:
+    if item in unique_item:
+        print("Duplicate: ", item)
+    unique_item.add(item)
+    
+        
